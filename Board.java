519258d5dcb410ec59a21535484559384546ac8d
AnthonyM456 <55559651+AnthonyM456@users.noreply.github.com>
2019-09-26 14:40:07 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,6 +1,12 @@
 
 package battleship;
 import java.awt.*;
+import java.io.File;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
 
 public class Board {
     private final static int NUM_CONNECT_WIN = 4;    
@@ -10,6 +16,12 @@ public class Board {
     private static Piece board[][][] = new Piece[NUM_ROWS][NUM_COLUMNS][2];
     private static int BoardSel;
 
+    private static Image WaterBgGif1;
+    private static Image WaterBgGif2;
+    
+    private static sound bgSound;
+    private static sound MisExplo;
+    
     public static void Reset() {
 //        thePiece = new Piece(Color.red);
 //clear the board.
@@ -29,6 +41,12 @@ public class Board {
         
         System.out.println(Window.getWidth2() + " " + Window.getHeight2());
         
+        if(bgSound.donePlaying)
+        bgSound = new sound("loadout_ambient.wav");
+    }
+    
+    public static void ExplosionSound(){
+        MisExplo = new sound("artillery_strike_far_01.wav"); 
     }
     
     public static void BoardSwitch(){
@@ -104,7 +122,9 @@ public class Board {
              
 
         board[zrow][zcol][BoardSel] = null;
-        Player.SwitchTurn();    
+        Board.BoardSwitch();
+        Player.SwitchTurn(); 
+        
         
         }
         public static int xdelta(){
@@ -129,11 +149,96 @@ public class Board {
                 
                 board[zrow][zcol][BoardSel] = new Piece(Player.GetCurrentPlayer().getColor());
 
-                
+                Board.BoardSwitch();
                 Player.SwitchTurn();
-            
+                
         }
 
     } 
+    
+    public static void PlayerPaint(Graphics2D g, Battleship thisObj){
+    if(Player.GetCurrentPlayer() == Player.getPlayer1()){        
+//fill background
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, Window.xsize, Window.ysize);
+
+        int x[] = {Window.getX(0), Window.getX(Window.getWidth2()), Window.getX(Window.getWidth2()), Window.getX(0), Window.getX(0)};
+        int y[] = {Window.getY(0), Window.getY(0), Window.getY(Window.getHeight2()), Window.getY(Window.getHeight2()), Window.getY(0)};
+//fill border
+//        g.setColor(Color.GRAY);
+//        g.fillPolygon(x, y, 4);
+        g.drawImage(WaterBgGif1,Window.getX(0),Window.getY(0),Window.getWidth2(),Window.getHeight2(),thisObj); 
+// draw border
+        g.setColor(Color.black);
+        g.drawPolyline(x, y, 5);
+}
+else if(Player.GetCurrentPlayer() == Player.getPlayer2())
+{
+//fill background
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, Window.xsize, Window.ysize);
+
+        int x[] = {Window.getX(0), Window.getX(Window.getWidth2()), Window.getX(Window.getWidth2()), Window.getX(0), Window.getX(0)};
+        int y[] = {Window.getY(0), Window.getY(0), Window.getY(Window.getHeight2()), Window.getY(Window.getHeight2()), Window.getY(0)};
+//fill border
+//        g.setColor(Color.blue);
+//        g.fillPolygon(x, y, 4);
+        g.drawImage(WaterBgGif2,Window.getX(0),Window.getY(0),Window.getWidth2(),Window.getHeight2(),thisObj); 
+
+// draw border
+        g.setColor(Color.black);
+        g.drawPolyline(x, y, 5);
+            
+}    
+    }    
+        
+    public static void Init(){
+        WaterBgGif1 = Toolkit.getDefaultToolkit().getImage("./WaterBgGif.gif");
+        WaterBgGif2 = Toolkit.getDefaultToolkit().getImage("./OceanBG.gif");
+        
+        bgSound = new sound("loadout_ambient.wav");
+    }
+////////////////////////////////////////////////////    
+    public static class sound implements Runnable {
+    Thread myThread;
+    File soundFile;
+    public boolean donePlaying = false;
+    sound(String _name)
+    {
+        soundFile = new File(_name);
+        myThread = new Thread(this);
+        myThread.start();
+        
+    }
+    public void run()
+    {
+        try {
+        AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);
+        AudioFormat format = ais.getFormat();
+//        System.out.println("Format: " + format);
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+        SourceDataLine source = (SourceDataLine) AudioSystem.getLine(info);
+        source.open(format);
+        source.start();
+        int read = 0;
+        byte[] audioData = new byte[16384];
+        while (read > -1){
+            read = ais.read(audioData,0,audioData.length);
+            if (read >= 0) {
+                source.write(audioData,0,read);
+            }
+        }
+        donePlaying = true;
+
+        source.drain();
+        source.close();
+        }
+        catch (Exception exc) {
+            System.out.println("error: " + exc.getMessage());
+            exc.printStackTrace();
+        }
+    }
+    }
+    
     
 }
