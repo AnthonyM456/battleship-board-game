622eb9dcba40fb155d7dabe01621132381d1b229
TomTatertot <55559669+TomTatertot@users.noreply.github.com>
2019-10-01 13:28:44 -0700

    Update Hover.java

--- Hover.java
+++ Hover.java
@@ -1,94 +1,223 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package battleship;
-import static battleship.Hover.hover;
-import java.awt.*;
 
-public class Board {
-    private final static int NUM_CONNECT_WIN = 4;    
-    
-    private final static int NUM_ROWS = 10;
-    private final static int NUM_COLUMNS = 10;      
-    public static Piece board[][] = new Piece[NUM_ROWS][NUM_COLUMNS];
-   
+import static battleship.Board.board;
+import java.awt.Color;
+import java.awt.Graphics2D;
 
-    public static void Reset() {
-//        thePiece = new Piece(Color.red);
-//clear the board.
-        for (int zrow=0;zrow<NUM_ROWS;zrow++)
-            for (int zcol=0;zcol<NUM_COLUMNS;zcol++)
-                board[zrow][zcol] = null;
-    }
+/**
+ *
+ * @author 145004436
+ */
+public class Hover {
+        private static boolean init;
+        public static Piece hover[][] = new Piece[Board.NUM_ROWS()][Board.NUM_COLUMNS()];
+        private static boolean rotate;
+        private static int mouseGetX;
+        private static int mouseGetY;
+        private static int shrink;
 
-    public static void Animate(){
-        int ydelta = Window.getHeight2()/NUM_ROWS;
-        int xdelta = Window.getWidth2()/NUM_COLUMNS;
-        
-    }
-    
-    public static void Draw(Graphics2D g) {
- //draw grid
-        int ydelta = Window.getHeight2()/NUM_ROWS;
-        int xdelta = Window.getWidth2()/NUM_COLUMNS;
- 
-        g.setColor(Color.black);
-        for (int zi = 1;zi<NUM_ROWS;zi++)
-        {
-            g.drawLine(Window.getX(0),Window.getY(zi*ydelta),
-                    Window.getX(Window.getWidth2()),Window.getY(zi*ydelta));
-        }
-        
-        for (int zi = 1;zi<NUM_COLUMNS;zi++)
-        {
-            g.drawLine(Window.getX(zi*xdelta),Window.getY(0),
-                    Window.getX(zi*xdelta),Window.getY(Window.getHeight2()));
-        }
-        for (int zrow=0;zrow<NUM_ROWS;zrow++)
-        {
-            for (int zcol=0;zcol<NUM_COLUMNS;zcol++)        
-            {
-                if (board[zrow][zcol] != null)
-                    board[zrow][zcol].draw(g, zrow, zcol, xdelta, ydelta);
-                    Player.SwitchTurn();
-            }
-        }
-    }
-    public static void RemovePiecePixel(int xpixel, int ypixel){
-        int xdelta = Window.getWidth2()/NUM_COLUMNS;
-        int ydelta = Window.getHeight2()/NUM_ROWS;
-        
+    public static void Highlight(int xpixel, int ypixel) {
+        int xdelta = Window.getWidth2()/10;
+        int ydelta = Window.getHeight2()/10;
         int zcol = (xpixel - Window.getX(0))/xdelta;
         int zrow = (ypixel - Window.getY(0))/ydelta;
-        
-        if (zcol == NUM_COLUMNS || zcol < 0 || zrow < 0 || zrow == NUM_ROWS) 
+        mouseGetX = xpixel;
+        mouseGetY = ypixel;
+        if (zcol == Board.NUM_COLUMNS() || zcol < 0 || xpixel < Window.getX(0) || ypixel < Window.getY(0)|| zrow < 0 || zrow == Board.NUM_ROWS())
             return;
-        
-        if (board[zrow][zcol] != null && board[zrow][zcol].getColor() != Player.GetCurrentPlayer().getColor())
+//        System.out.println(zcol+"             "+zrow);
+        if (!rotate)
         {
-            System.out.println("gotem");
-            int zchangeRow = zrow;
-            while (zchangeRow > 0 && board[zchangeRow-1][zcol] != null)
+            if (zrow > 1)
             {
-                board[zchangeRow][zcol] = board[zchangeRow-1][zcol];
-                zchangeRow--;
+                hover[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    hover[zrow-1][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (shrink < 1)
+                        hover[zrow-2][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+
             }
-            board[zchangeRow][zcol] = null;
+            else if (zrow == 1) {
+                hover[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    hover[zrow-1][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (shrink < 1)
+                        hover[zrow+1][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+            }
+            else if (zrow == 0) {
+                hover[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    hover[zrow+1][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (shrink < 1)
+                        hover[zrow+2][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+            }
+        }
+        else 
+        {
+            if (zcol > 1)
+            {
+                hover[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    hover[zrow][zcol-1] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (shrink < 1)
+                        hover[zrow][zcol-2] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+
+            }
+            else if (zcol == 1) {
+                hover[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    hover[zrow][zcol-1] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (shrink < 1)
+                        hover[zrow][zcol+1] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+            }
+            else if (zcol == 0) {
+                hover[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    hover[zrow][zcol+1] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (shrink < 1)
+                        hover[zrow][zcol+2] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+            }
+        }
+    }
+    public static void Click(){
+        shrink++;
+        if (shrink > 2)
+        {
             Player.SwitchTurn();
+            shrink = 0;
+        }
+        System.out.println(shrink+"");
+        
+    }
+    public static void PlaceShip(int xpixel,int ypixel) {
+        
+        int ydelta = Window.getHeight2()/Board.NUM_ROWS();
+        int xdelta = Window.getWidth2()/Board.NUM_COLUMNS();
+
+        int zcol = (xpixel-Window.getX(0))/xdelta;
+        int zrow = (ypixel-Window.getY(0))/ydelta;
+
+        if (!Hover.getRotate())
+        {
+            if (zrow > 1)
+            {
+                board[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    board[zrow-1][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (Hover.getShrink() < 1)
+                        board[zrow-2][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+
+            }
+            else if (zrow == 1) {
+                board[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    board[zrow-1][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (shrink < 1)
+                        board[zrow+1][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+            }
+            else if (zrow == 0) {
+                board[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    board[zrow+1][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (shrink < 1)
+                        board[zrow+2][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+            }
+        }
+        else 
+        {
+            if (zcol > 1)
+            {
+                board[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    board[zrow][zcol-1] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (shrink < 1)
+                        board[zrow][zcol-2] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+
+            }
+            else if (zcol == 1) {
+                board[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    board[zrow][zcol-1] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (shrink < 1)
+                        board[zrow][zcol+1] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+            }
+            else if (zcol == 0) {
+                board[zrow][zcol] = new Piece(Player.GetCurrentPlayer().getColor());
+                if (shrink < 2)
+                {
+                    board[zrow][zcol+1] = new Piece(Player.GetCurrentPlayer().getColor());
+                    if (shrink < 1)
+                        board[zrow][zcol+2] = new Piece(Player.GetCurrentPlayer().getColor());
+                }
+            }
+        }
+    }
+    public static void Move(){
+        for (int zrow=0;zrow<Board.NUM_ROWS();zrow++)
+        {
+            for (int zcol=0;zcol<Board.NUM_COLUMNS();zcol++)        
+            {
+                hover[zrow][zcol] = null;
+            }
+        }
+    }
+    public static void Draw(Graphics2D g) {
+        int ydelta = Window.getHeight2()/Board.NUM_COLUMNS();
+        int xdelta = Window.getWidth2()/Board.NUM_ROWS();
+        
+        for (int zrow=0;zrow<Board.NUM_ROWS();zrow++) {
+            for (int zcol=0;zcol<Board.NUM_COLUMNS();zcol++)   {
+                if (hover[zrow][zcol] != null)
+                    hover[zrow][zcol].draw(g, zrow, zcol, xdelta, ydelta);
+            }
         }
         
     }
-    public static int xdelta(){
-        int xdelta = Window.getWidth2()/NUM_COLUMNS;
-        return(xdelta);
+    public static int mouseGetX() {
+        return(mouseGetX);
     }
-    public static int ydelta(){
-        int ydelta = Window.getHeight2()/NUM_ROWS;
-        return(ydelta);
+    public static int mouseGetY() {
+        return(mouseGetY);
     }
-    public static int NUM_ROWS(){
-        return(NUM_ROWS);
+    public static int getShrink(){
+        return(shrink);
     }
-    public static int NUM_COLUMNS(){
-        return(NUM_COLUMNS);
+    public static void rotateShip(){
+        
+        if (rotate == true)
+            rotate = false;
+        else
+            rotate = true;
     }
-//    
+    public static boolean getRotate(){
+        return(rotate);
+    }
+    
 }
